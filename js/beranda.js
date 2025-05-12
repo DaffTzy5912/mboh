c9a6ec074d50199aaf2c7a9463b21872280a42ab
DaffTzy5912 <dafanation999@gmail.com>
2025-05-12 14:53:02 +0700

    Create beranda.js

---
+++ js/beranda.js
@@ -0,0 +1,5 @@
+
+document.querySelector(".beranda").innerHTML += `
+  <img src='https://via.placeholder.com/150' alt='Profile' style='border-radius: 50%; margin-top: 1rem;' />
+  <p style='margin-top: 1rem;'>Saya seorang web developer yang menyukai desain modern dan pengembangan interaktif.</p>
+`;
